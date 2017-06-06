0baaec85463c4910aec0ab7536fbd4c0109cc5b0
Jackie Amador <jamador01@saintmarys.edu>
2017-06-06 14:06:27 -0400

    Added name to details

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ function handleSubmit(ev) {
     ev.preventDefault()
     const f = ev.target
     const x = ev.target //assigned to the remaining const pulling the target of the querySelector
-    const heading = document.querySelector('h1')
-    heading.textContent = f.personName.value + ', ' +  x.personPlace.value 
+    //const heading = document.querySelector('h1')
 
+    const details = document.querySelector('#details')
+    const name = f.personName.value
+    details.innerHTML += '<strong>'+ name + '</strong>'
+
+    heading.textContent = f.personName.value + ', ' +  x.personPlace.value 
    
 }
 
 personForm.addEventListener('submit', handleSubmit)
+
+
 
 
