a0ea5fc04674bb3f14acc81efecea428f23bff5b
Jackie Amador <jamador01@saintmarys.edu>
2017-06-06 15:38:51 -0400

    Add form values using form values

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
 
 const personForm = document.querySelector('#personForm')
-const personPlace = document.querySelector('#personPlace')
+//const personPlace = document.querySelector('#personPlace')
 
 function handleSubmit(ev) {
     ev.preventDefault()
     const f = ev.target
-    const x = ev.target //assigned to the remaining const pulling the target of the querySelector
-    const heading = document.querySelector('h1')
+    //const x = ev.target //assigned to the remaining const pulling the target of the querySelector
+    //const heading = document.querySelector('h1')
 
     const details = document.querySelector('#details')
     const name = f.personName.value
     
 
-    //const boldedName = document.createElement('strong')
-    //boldedName.textContent = name
-    //details.appendChild(boldedName)
+    //const boldedName = document.createElement('strong')//creates the empty strong element
+    //boldedName.textContent = name//puts name inside empty strong element
+    //details.appendChild(boldedName) // sticks inside the details div
+
     const favoriteColor = f.favoriteColor.value
     const age = f.age.value
+
     const colorDiv = `<div style="background-color: ${favoriteColor}; width: 100px; height: 50px; "></div>`
 
+    const thing = document.createElement(``)
+    thing.textContent = 
+    details.appendChild(thing)
+
+    const list = document.createElement('ul')
+    const nameItem = document.createElement('li')
+    nameItem.textContent = `Name: ${name}`
+   
+    const colorItem = document.createElement('li')
+    colorItem.innerHTML = `Favorite Color: ${colorDiv}`
+   
+    const ageItem = document.createElement('li')
+    ageItem.textContent = `Age: ${age}`
+   
+    list.appendChild(nameItem)
+    list.appendChild(colorItem)
+    list.appendChild(ageItem)
+    details.appendChild(list)
+   
     details.innerHTML = `
     <ul>
         <li> Name: ${name} </li>
@@ -27,6 +48,7 @@ function handleSubmit(ev) {
 
     </ul>
     `
+     
 
 
 //can't take boldedName and place directly below to grab the element
