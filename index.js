60a52e28e41add1a4801b009a44369bdf6c8349f
Jackie Amador <jamador01@saintmarys.edu>
2017-06-06 07:29:07 -0400

    Use of both input values in h1

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-//console.log('It Worked')
-
 
 const personForm = document.querySelector('#personForm')
+const personPlace = document.querySelector('#personPlace')
 
 function handleSubmit(ev) {
     ev.preventDefault()
     const f = ev.target
+    const x = ev.target //assigned to the remaining const pulling the target of the querySelector
     const heading = document.querySelector('h1')
-    heading.textContent = f.personName.value 
-    
-    //console.log(f.personName.value)
-    //console.log('Submittt~')
+    heading.textContent = f.personName.value + ', ' +  x.personPlace.value 
+
+   
 }
 
 personForm.addEventListener('submit', handleSubmit)
